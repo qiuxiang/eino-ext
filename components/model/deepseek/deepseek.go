4f487ae96b17fcd0603de4233e28ac4ca40b7210
7c00 <i@7c00.cc>
2025-04-22 15:35:39 +0800

    fix: deepseek stream options include usage

--- components/model/deepseek/deepseek.go
+++ components/model/deepseek/deepseek.go
@@ -484,6 +484,7 @@ func (cm *ChatModel) generateStreamRequest(ctx context.Context, in []*schema.Mes
 	}
 	req := &deepseek.StreamChatCompletionRequest{
 		Stream:           true,
+		StreamOptions:    deepseek.StreamOptions{IncludeUsage: true},
 		Model:            origReq.Model,
 		Messages:         origReq.Messages,
 		FrequencyPenalty: origReq.FrequencyPenalty,
